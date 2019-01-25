dfff93148d176a8e14e3afbb5cee9e7b2fd357b7
Pamela Abreu <pamelaabreu@pursuit.com>
2019-01-24 21:09:01 -0500

    initial db

---
+++ backend/services/db.js
@@ -0,0 +1,12 @@
+// singleton pattern
+
+let pgp = null;
+let db = null;
+
+if (!pgp) {
+    pgp = require('pg-promise')({});  //require, and invoking with default empty object
+    db = pgp('postgres://localhost/petpedia'); //invoking pgp with empty string
+}
+
+
+module.exports = {db}
