b670c0a8f8f95335a6242ce5d54a9b600f5f4c6a
Pamela Abreu <pamelaabreu@pursuit.com>
2019-01-24 21:58:59 -0500

    added db and fixed db with blog

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,11 +1,10 @@
 // singleton pattern
-
 let pgp = null;
 let db = null;
 
 if (!pgp) {
     pgp = require('pg-promise')({});  //require, and invoking with default empty object
-    db = pgp('postgres://localhost/petpedia'); //invoking pgp with empty string
+    db = pgp('postgres://localhost/blog'); //invoking pgp with empty string
 }
 
 
