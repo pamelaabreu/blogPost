0e0b5047d6b2787bb5e1397e7122de63ec251529
Pamela Abreu <pamelaabreu@pursuit.com>
2019-01-29 16:53:24 -0500

    created post route for post, no auth

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -13,7 +13,7 @@ postRoutes.get('/:post_id', (req, res) => {
         })
         .catch(err => {
             console.log('Error', err.toString());
-            res.status(404).json({error: `Unable to find user! Try again!`});
+            res.status(404).json({error: `Unable to find post! Try again!`});
         })
 });
 
@@ -27,14 +27,41 @@ postRoutes.get('/:post_id/comments', (req, res) => {
         })
         .catch(err => {
             console.log('Error', err.toString());
-            res.status(404).json({error: `Unable to find user! Try again!`});
+            res.status(404).json({error: `Unable to find post comments! Try again!`});
         })
 });
 
 // ✅ GET /post/:post_id/comments/:comment_id
+postRoutes.get('/:post_id/comments/:comment_id', (req, res) => {
+    const { post_id, comment_id } = req.params;
+
+    postService.readComments(post_id, comment_id)
+        .then(data => {
+            res.json({ data });
+        })
+        .catch(err => {
+            console.log('Error', err.toString());
+            res.status(404).json({error: `Unable to find post comments! Try again!`});
+        })
+});
 
 // PRIVATE POST ROUTES
 // ❌ POST /post
+postRoutes.post('/', (req, res) => {
+    const { author, title, body } = req.body;
+
+    postService.create(author, title, body)
+    .then(() => {
+        res.json({success: `Created ${title} by ${author}.`});
+    })
+    .catch(err => {
+        console.log('Error', err.toString());
+        res.status(404).json({error: `Unable to create ${title}! Try again!`});
+    })
+});
+
+
+
 // ❌ PUT /post/:post_id
 // ❌ DEL /post/:post_id
 
