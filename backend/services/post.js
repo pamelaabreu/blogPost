1d1ef596eb6ea44e3e9cf65977bd4af8fa05a199
Pamela Abreu <pamelaabreu@pursuit.com>
2019-01-29 16:54:11 -0500

    created function for inserting into post table

--- backend/services/post.js
+++ backend/services/post.js
@@ -10,13 +10,30 @@ postService.read = (post_id) => {
 
 };
 
-postService.readComments = (post_id) => {
+postService.readComments = (post_id, comment_id) => {
     const sql = `
     SELECT * FROM comment WHERE post_id=$[post_id];
     `;
 
+    const sql2 = `
+    SELECT * FROM comment WHERE post_id=$[post_id] AND id=$[comment_id];
+    `;
+
+    if(comment_id){
+        return db.one(sql2, { user_id, comment_id });
+    }
+
     return db.any(sql, { post_id }); 
 
+};
+
+postService.create = (author, title, body) => {
+    const sql = `
+    INSERT INTO posts (author, title, body) 
+    VALUES ($[author], $[title], $[body]);
+    `;
+    
+    return db.none(sql, { author, title, body });
 };
 
 
