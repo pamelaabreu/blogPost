77a516e3239cf20414eb4a4e989bdb6d709de177
Pamela Abreu <pamelaabreu@pursuit.com>
2019-01-31 20:27:09 -0500

    Added /comment post and put routes

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -19,8 +19,34 @@ commentRoutes.get('/:comment_id', (req, res) => {
 });
 
 // ❌ POST /comment
+commentRoutes.post('/', (req, res) => {
+    const { author, post_id, title, body } = req.body;
+
+    commentService.create(author, post_id, title, body)
+        .then(() => {
+            res.json({success: `Created comment ${title}!`});
+        })
+        .catch(err => {
+            console.log('Error', err.toString());
+            res.status(404).json({error: `Unable to create comment! Try again!`});
+        })
+});
 
 // ❌ PUT /comment/:comment_id
+commentRoutes.put('/:comment_id', (req, res) => {
+    const { author, post_id, title, body } = req.body;
+    const { comment_id } = req.params;
+
+    commentService.update(author, post_id, title, body, comment_id )
+        .then(() => {
+            res.json({success: `Update comment ${title}!`});
+        })
+        .catch(err => {
+            console.log('Error', err.toString());
+            res.status(404).json({error: `Unable to update comment! Try again!`});
+        })
+});
+
 // ❌ DEL /comment/:comment_id
 
 module.exports = commentRoutes;
