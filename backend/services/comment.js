d9e3bf7b59d4e9371c463cf77c58641f052f808e
Pamela Abreu <pamelaabreu@pursuit.com>
2019-01-31 20:27:47 -0500

    added create and update functions

--- backend/services/comment.js
+++ backend/services/comment.js
@@ -10,4 +10,21 @@ commentService.read = (comment_id) => {
     
 };
 
+commentService.create = (author, post_id, title, body) => {
+    const sql = `
+    INSERT INTO comment (author, post_id, title, body) 
+    VALUES ($[author], $[post_id], $[title], $[body]);
+    `;
+    
+    return db.none(sql, { author, post_id, title, body });
+};
+
+commentService.update = (author, post_id, title, body, comment_id ) => {
+    const sql = 'UPDATE comment SET author=${author}, post_id=${post_id}, title=${title}, body=${body} WHERE id=${comment_id};';
+    return db.none(sql, { author, post_id, title, body, comment_id });
+};
+
+
+
+
 module.exports = commentService;
